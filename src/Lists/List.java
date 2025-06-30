db53acde23b76c9ade59a5058ca73b6c89a4dbab
Marco Vinicio Vasquez Barrantes <mavasquezba@est.utn.ac.cr>
2025-06-30 14:19:26 -0600

    Creacion clase List
    Se creo la interfaz List para utilizar en el proyecto para las listas que cada clase requiere

---
+++ src/Lists/List.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package Lists;
+
+/**
+ *
+ * @author autoa
+ */
+public interface List <T>{
+    
+    public boolean agregar(T t);
+    
+    public boolean eliminar(T t);
+    
+    public T buscar(T t);
+}
