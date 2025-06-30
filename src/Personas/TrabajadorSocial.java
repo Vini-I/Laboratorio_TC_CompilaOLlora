e107ba65b7af3aeb6e89dff882445179d0656a42
Leandro Sanchez Rojas <lesanchezro@est.utn.ac.cr>
2025-06-30 14:52:03 -0600

    Clase Trabajador Social
    Se realizo la clase trabajadorsocial que hereda de persona con sus respectivos atributos y metodos

---
+++ src/Personas/TrabajadorSocial.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+/**
+ *
+ * @author llean
+ */
+public class TrabajadorSocial extends Persona {
+    private String canton;
+    
+    //getters
+
+    public String getCanton() {
+        return canton;
+    }
+    
+    //setters
+
+    public void setCanton(String canton) {
+        this.canton = canton;
+    }
+    
+    //constructor
+
+    public TrabajadorSocial(String canton, String cedula, String nombre, String correo, String telefono) {
+        super(cedula, nombre, correo, telefono);
+        this.canton = canton;
+    }
+    
+    //toString
+
+    @Override
+    public String toString() {
+        return "TrabajadorSocial"+ super.toString() +"canton=" + canton;
+    }
+    
+    
+    
+}
