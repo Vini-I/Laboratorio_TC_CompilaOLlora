570bf3e66ac33d52d77f9d47de347a6baddc5a04
Joches29 <jochaveshe@est.utn.ac.cr>
2025-06-30 15:46:30 -0600

    Se crea la clase ListaVictima
    Se agrega la clase ListaVictima, con su implementacion de la interface List, se crea con su arreglo, constructor para definir el tamaño, y sus metodo genericos, con el metodo de buscar programado

---
+++ src/Personas/Victima/ListaVictima.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas.Victima;
+
+import Lists.List;
+
+/**
+ *
+ * @author rodol
+ */
+public class ListaVictima implements List <Victima> {
+    
+    Victima [] listaVictimas;
+
+    public ListaVictima() {
+        this.listaVictimas = new Victima[200];
+    }
+    @Override
+    public boolean agregar(Victima t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean eliminar(Victima t) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public Victima buscar(Victima t) {
+        int max = listaVictimas.length;
+        for (int i = 0; i < max; i++) {
+            if (listaVictimas[i].getCedula().equalsIgnoreCase(t.getCedula())){
+                return listaVictimas[i];
+            }
+        }
+        return null;
+    }
+    
+    
+    
+}
