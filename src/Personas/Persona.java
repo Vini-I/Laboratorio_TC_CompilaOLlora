c214e0c3e79231da7e9a3d6c0f464d6741de0f71
Joches29 <jochaveshe@est.utn.ac.cr>
2025-06-30 14:37:22 -0600

    Se crea el paquete Personas y clase Persona
    Se agrega la clase Persona con los atributos comunes especificados en el enunciado

---
+++ src/Personas/Persona.java
@@ -0,0 +1,53 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Personas;
+
+/**
+ *
+ * @author rodol
+ */
+public abstract class Persona {
+    
+    private String cedula;
+    private String nombre;
+    private String correo;
+    private String telefono;
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Persona(String cedula, String nombre, String correo, String telefono) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    @Override
+    public String toString() {
+        return "Cedula: " + cedula + " Nombre: " + nombre + " Correo: " + correo + " Telefono: " + telefono;
+    }
+}
